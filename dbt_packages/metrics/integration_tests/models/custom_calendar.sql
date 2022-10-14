48706546ac6aa64ab33cfe8631d2730f268fdd6d
Mirna Wong <mirna.wong@fishtownanalytics.com>
2022-10-14 17:45:15 +0000

    test

--- dbt_packages/metrics/integration_tests/models/custom_calendar.sql
+++ dbt_packages/metrics/integration_tests/models/custom_calendar.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 with days as (
-    {{ dbt_utils.date_spine(
+    {{ metrics.metric_date_spine(
     datepart="day",
     start_date="cast('2010-01-01' as date)",
     end_date="cast('2030-01-01' as date)"
@@ -14,13 +14,13 @@ final as (
         cast(date_day as date) as date_day,
         {% if target.type == 'bigquery' %}
             --BQ starts its weeks on Sunday. I don't actually care which day it runs on for auto testing purposes, just want it to be consistent with the other seeds
-            cast({{ dbt_utils.date_trunc('week(MONDAY)', 'date_day') }} as date) as date_week,
+            cast({{ date_trunc('week(MONDAY)', 'date_day') }} as date) as date_week,
         {% else %}
-            cast({{ dbt_utils.date_trunc('week', 'date_day') }} as date) as date_week,
+            cast({{ date_trunc('week', 'date_day') }} as date) as date_week,
         {% endif %}
-        cast({{ dbt_utils.date_trunc('month', 'date_day') }} as date) as date_month,
-        cast({{ dbt_utils.date_trunc('quarter', 'date_day') }} as date) as date_quarter,
-        cast({{ dbt_utils.date_trunc('year', 'date_day') }} as date) as date_year,
+        cast({{ date_trunc('month', 'date_day') }} as date) as date_month,
+        cast({{ date_trunc('quarter', 'date_day') }} as date) as date_quarter,
+        cast({{ date_trunc('year', 'date_day') }} as date) as date_year,
         true as is_weekend
     from days
 )
--- dbt_packages/metrics/tests/functional/fixtures.py
+++ dbt_packages/metrics/tests/functional/fixtures.py
@@ -46,7 +46,7 @@ purchased_at,payment_type,payment_total
 custom_calendar_sql = """
 {{ config(materialized='table') }}
 with days as (
-    {{ dbt_utils.date_spine(
+    {{ metrics.metric_date_spine(
     datepart="day",
     start_date="cast('2010-01-01' as date)",
     end_date="cast('2030-01-01' as date)"
@@ -58,13 +58,13 @@ final as (
         cast(date_day as date) as date_day,
         {% if target.type == 'bigquery' %}
             --BQ starts its weeks on Sunday. I don't actually care which day it runs on for auto testing purposes, just want it to be consistent with the other seeds
-            cast({{ dbt_utils.date_trunc('week(MONDAY)', 'date_day') }} as date) as date_week,
+            cast({{ date_trunc('week(MONDAY)', 'date_day') }} as date) as date_week,
         {% else %}
-            cast({{ dbt_utils.date_trunc('week', 'date_day') }} as date) as date_week,
+            cast({{ date_trunc('week', 'date_day') }} as date) as date_week,
         {% endif %}
-        cast({{ dbt_utils.date_trunc('month', 'date_day') }} as date) as date_month,
-        cast({{ dbt_utils.date_trunc('quarter', 'date_day') }} as date) as date_quarter,
-        cast({{ dbt_utils.date_trunc('year', 'date_day') }} as date) as date_year,
+        cast({{ date_trunc('month', 'date_day') }} as date) as date_month,
+        cast({{ date_trunc('quarter', 'date_day') }} as date) as date_quarter,
+        cast({{ date_trunc('year', 'date_day') }} as date) as date_year,
         true as is_weekend
     from days
 )
@@ -147,5 +147,42 @@ models:
 # packages.yml
 packages_yml = """
   - package: calogica/dbt_expectations
-    version: [">=0.5.0", "<0.6.0"]
+    version: [">=0.6.0", "<0.7.0"]
+
+  - package: dbt-labs/dbt_utils
+    version: [">=0.9.0", "<1.0.0"]
+"""
+
+# seeds/events.csv
+events_source_csv = """
+id,country,timestamp_field
+1,FR,2022-01-01
+2,UK,2022-02-01
+""".lstrip()
+
+# models/event.sql
+event_sql = """
+with source as (
+    select * from {{ ref('events_source') }}
+)
+,
+final as (
+    select *
+    from source 
+)
+select * from final
+"""
+
+# models/event.yml
+event_yml = """
+version: 2 
+models: 
+  - name: event
+    columns:
+      - name: id
+        description: TBD
+      - name: country
+        description: TBD
+      - name: timestamp_field
+        description: TBD
 """
